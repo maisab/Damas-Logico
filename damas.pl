ef89a9013221e2230240dfe6ae698b6a2fbafae5
maisab <maaisa.barreto@gmail.com>
2016-06-27 18:36:10 -0300

    Movendo

--- damas.pl
+++ damas.pl
@@ -1,4 +1,8 @@
-tabuleiroInicial(X) :- X =	[[pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida],
+tabuleiroInicial(X) :- mostraTabuleiro(X), movePeca(X, 2,0, 3,1, Novo), nl, mostraTabuleiro(Novo).
+%jogar(X, M), tabuleiroInicial(M)
+
+
+/*X =	[[pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida],
 		                     [invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP],
 		                     [pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida],
 		                     [invalida, vazia, invalida, vazia, invalida, vazia, invalida, vazia],
@@ -7,7 +11,7 @@ tabuleiroInicial(X) :- X =	[[pecaP, invalida, pecaP, invalida, pecaP, invalida, 
 		                     [pecaC, invalida, pecaC, invalida, pecaC, invalida, pecaC, invalida],
 		                     [invalida, pecaC, invalida, pecaC, invalida, pecaC, invalida, pecaC]].
 
-
+*/
 %Regras para exibir o tabuleiro
 mostrar(pecaP, 'p').
 mostrar(pecaC, 'c').
@@ -26,12 +30,19 @@ peca_valida(pecaP).
 peca_valida(pecaC).
 
 inicializar :- 
-	tabuleiroInicial(X), 
-	assert(tabuleiro(X)),
-	mostraTabuleiro(X),
-	jogar(X).
+	tabuleiroInicial([[pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida],
+	                 [invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP],
+	                 [pecaP, invalida, pecaP, invalida, pecaP, invalida, pecaP, invalida],
+	                 [invalida, vazia, invalida, vazia, invalida, vazia, invalida, vazia],
+	                 [vazia, invalida, vazia, invalida, vazia, invalida, vazia, invalida],
+	                 [invalida, pecaC, invalida, pecaC, invalida, pecaC, invalida, pecaC],
+	                 [pecaC, invalida, pecaC, invalida, pecaC, invalida, pecaC, invalida],
+	                 [invalida, pecaC, invalida, pecaC, invalida, pecaC, invalida, pecaC]]).
+/*	assert(tabuleiro(X)), 
+	mostraTabuleiro(X), nl,*/
+%	jogar(X).
 
-jogar(T) :- trocaPosicao(T, 2, 2, invalida, Novo), write(Novo), nl,  mostraTabuleiro(Novo).
+jogar(T) :- movePeca(T, 2,0, 3,1, Novo), mostraTabuleiro(Novo).
 
 %-----------------posicaoValidaJogador---------------------
 posicaoValidaJogador(X,Y, DestinoX, DestinoY) :- 
@@ -60,7 +71,7 @@ posicaoValidaComerComputador(DestinoX, DestinoY) :- %verifica se pode comer
 %-----------------imprimirTabuleiro -------------
 mostraTabuleiro(T) :- 
 	write('     0   1   2   3   4   5   6   7	'), nl,
-    tabuleiro(T), imprimirLinha(0, T),!.
+    imprimirLinha(0, T),!.
                 
 imprimirLinha(8,_). 
 imprimirLinha(Pos,[Hd|Ht]) :- 
@@ -69,10 +80,10 @@ imprimirLinha(Pos,[Hd|Ht]) :-
 
 imprimirColuna([]).                            
 imprimirColuna([Hd|Ht]) :- 
-      mostrar(Hd, X),write(' | '),write(X), imprimirColuna(Ht).
+      mostrar(Hd,X), write(' | '),write(X), imprimirColuna(Ht).
 
 %-----------------encontraPosicao -------------
-encontraPosicao(Linha,Coluna,E) :- tabuleiro(X), encontraLinha(Linha, Coluna, X, E), !.
+encontraPosicao(T,Linha,Coluna,E) :- encontraLinha(Linha, Coluna, T, E), !.
 
 encontraLinha(0, J, [H|_], E):- encontraColuna(J, H, E). %quando encontrar a linha, vai para a coluna
 encontraLinha(I, J, [_|T], E):- X is I - 1, encontraLinha(X, J, T, E). %decrementa ate chegar na posicao correta
@@ -82,9 +93,7 @@ encontraColuna(J, [_|T], E):- X is J - 1, encontraColuna(X, T, E). %decrementa a
 
 %-----------------trocaPosicao ---------------
 trocaPosicao(T,X,Y,NovaPeca, NovoTabuleiro) :- 
-	tabuleiro(T), retract(tabuleiro(_)),  %remove o tabuleiro atual
-    trocaLista(X,Y,NovaPeca,T,NovoTabuleiro),
-    assert(tabuleiro(NovoTabuleiro)). %atualiza o tabuleiro
+    trocaLista(X,Y,NovaPeca,T,NovoTabuleiro).
                       
 trocaLista(0,Y,NovaPeca,[H|T],[NovoTabuleiro|T]) :- trocaColuna(Y,NovaPeca,H,NovoTabuleiro).
 trocaLista(X,Y,NovaPeca,[H|T],[H|NovoTabuleiro]) :- AuxX is X - 1, trocaLista(AuxX,Y,NovaPeca,T,NovoTabuleiro).
@@ -93,16 +102,16 @@ trocaColuna(0,NovaPeca,[_|T], [NovaPeca|T]).
 trocaColuna(Y,NovaPeca,[H|T],[H|NovoTabuleiro]) :- AuxY is Y - 1, trocaColuna(AuxY,NovaPeca,T,NovoTabuleiro).
 
 %-------------movePeca-------------------
-movePeca(X, Y, DestinoX, DestinoY) :-
-	DestinoX is X + 1, 
+movePeca(T, X, Y, DestinoX, DestinoY, NovoTabuleiro) :-
+	DestinoX is X + 1,
 	posicaoValidaJogador(X, Y, DestinoX, DestinoY), 
-	encontraPosicao(X, Y, PAtual), peca_valida(PAtual), 
-	encontraPosicao(DestinoX, DestinoY, C), casa_valida(C),
-	trocaPosicao(X,Y,vazia),
-	trocaPosicao(DestinoX, DestinoY, PAtual), mostraTabuleiro.
+	encontraPosicao(T, X, Y, PAtual), peca_valida(PAtual),
+	encontraPosicao(T, DestinoX, DestinoY, C), casa_valida(C),
+	trocaPosicao(T, X, Y,vazia, Novo),
+	trocaPosicao(Novo, DestinoX, DestinoY, PAtual, NovoTabuleiro).
 
 %-------------movePecaComputador-------------------
-movePeca(X, Y, DestinoX, DestinoY) :-
+/*movePeca(X, Y, DestinoX, DestinoY) :-
 	DestinoX is X - 1,
 	posicaoValidaComputador(X, Y, DestinoX, DestinoY), 
 	encontraPosicao(X, Y, PAtual), peca_valida(PAtual), 
@@ -165,3 +174,4 @@ comePeca(X, Y, DestinoX, DestinoY) :-
 	trocaPosicao(DestinoX,DestinoY,vazia),
 	NovoX is DestinoX - 1, NovoY is DestinoY - 1,
 	trocaPosicao(NovoX, NovoY, PAtual), mostraTabuleiro.
+*/
